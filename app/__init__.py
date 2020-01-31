ad18d630dcca93b397a4306b06132bc1022d2b78
nnorukaemeka <nnorukaemeka@gmail.com>
2020-01-31 18:53:51 +0100

    heroku structure changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask_uuid import FlaskUUID
+from flask_restful import Resource, Api
+import uuid
 
 # Initialize application
 app = Flask(__name__, static_folder=None)
@@ -13,8 +15,6 @@ app_settings = os.getenv(
 )
 app.config.from_object(app_settings)
 
-# Initialize Bcrypt
-bcrypt = Bcrypt(app)
 
 # Initialize Flask Sql Alchemy
 db = SQLAlchemy(app)
@@ -22,19 +22,3 @@ db = SQLAlchemy(app)
 # Import the application views
 from app import views
 
-# Register blue prints
-from app.auth.views import auth
-
-app.register_blueprint(auth)
-
-from app.bucket.views import bucket
-
-app.register_blueprint(bucket)
-
-from app.bucketitems.views import bucketitems
-
-app.register_blueprint(bucketitems)
-
-from app.docs.views import docs
-
-app.register_blueprint(docs)
--- app/views.py
+++ app/views.py
@@ -1,32 +1,63 @@
-from app import app
-from app.bucketitems.helper import response
+from flask import render_template, request, flash, url_for, redirect, session
+from app import app, db, api
+from app.models import PinGenerator
+from flask_restful import Resource, Api
+from sqlalchemy import exc
+from functools import wraps
+from sqlalchemy.exc import IntegrityError
+import uuid
 
 
-@app.errorhandler(404)
-def route_not_found(e):
-    """
-    Return a custom 404 Http response message for missing or not found routes.
-    :param e: Exception
-    :return: Http Response
-    """
-    return response('failed', 'Endpoint not found', 404)
+def random_pin(length):
+    stringLength = length
+    randomString = str(uuid.uuid4().int) # get a random string in a UUID fromat
+    randomString  = randomString[0:stringLength] # trim to stringLength.
+    return randomString
 
 
-@app.errorhandler(405)
-def method_not_found(e):
-    """
-    Custom response for methods not allowed for the requested URLs
-    :param e: Exception
-    :return:
-    """
-    return response('failed', 'The method is not allowed for the requested URL', 405)
+class Generate(Resource):
+    def get(self):
+        try:
+            #generate a random uuid 15 digits
+            epin = random_pin(15)
+            #generate a random uuid 25 digits
+            eserial_no = random_pin(25)
+            #make an object of the database class
+            new_pin = PinGenerator(pin = epin, serial_no = eserial_no)
+
+            #compare the generated digits with database, if any exists, generate again
+            if PinGenerator.query.get(epin) or PinGenerator.query.filter_by(serial_no= eserial_no).first():
+                db.session.rollback()
+                return redirect(url_for('generate'))
+            # if they don't exist in db, add to database and return the generated digits
+            db.session.add(new_pin)
+            db.session.commit()
+            return {"pin": epin, "s/n": eserial_no}, 200
+        except exc.IntegrityError: #for any other exception, flag error message
+            db.session.rollback()
+            return {"message": " Oops! try again later"}, 404
+
+api.add_resource(Generate, '/generate')  
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    """
-    Return a custom message for a 500 internal error
-    :param e: Exception
-    :return:
-    """
-    return response('failed', 'Internal server error', 500)
+class Validate(Resource):
+    def post(self):
+        #get serial_no and pin from user, and convert to string
+        request_data = request.get_json()
+        eserial_no = str(request_data['s/n'])
+        epin = str(request_data['pin'])
+        #check if both serial_no and pin exist in the database
+        chkpin = PinGenerator.query.filter_by(pin=epin, serial_no=eserial_no).first()
+        if chkpin: # if they exist, return 1 connoting valid
+            return {'message':"1"},200
+        else: # if they don't exist, return 0 connoting invalid
+            return {'message':"0"}, 200
+
+api.add_resource(Validate, '/validate')
+
+
+class Home(Resource):
+    def get(self):
+        return {
+            'message': '''Welcome, my name is Emeka Nnoruka. The purpose of this project is to build two(2) API. This first endpoint ["/generate"] will return a PIN and a SERIAL_NO, while the second endpoint ["/validate"] will request for SERIAL_NO and PIN and validate if they are VALID [1] or NOT [0].Feel free to consume the APIs.'''}
+api.add_resource(Home, '/')
