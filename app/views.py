4c8e1282ba603683028493d66ca8cf6c690b0a3c
nnorukaemeka <nnorukaemeka@gmail.com>
2020-02-03 14:28:21 +0100

    validate route updated

--- app/views.py
+++ app/views.py
@@ -89,5 +89,5 @@ api.add_resource(AllPin, '/database')
 class Home(Resource):
     def get(self):
         return {
-            'message': '''Welcome, my name is Emeka Nnoruka. The purpose of this project is to build two(2) API. This first endpoint [/generate] returns a pin and a serial_no(s/n). The second endpoint [/validate/pin/  or /validate/sn/] validates pin or s/n when posted by returning [1] as VALID or [0] as NOT VALID. I added additional endpoint [/database] for accessing the whole data in the database. Feel free to consume the APIs.'''}
+            'message': '''Welcome, my name is Emeka Nnoruka. The purpose of this project is to build two(2) API. This first endpoint [/generate] returns a pin and a serial_no(s/n). The second endpoint [/validate/pin/...  or /validate/sn/...] validates pin or s/n when posted by returning [1] as VALID or [0] as NOT VALID. I added additional endpoint [/database] for accessing the whole data in the database. Feel free to consume the APIs.'''}
 api.add_resource(Home, '/')
