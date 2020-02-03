049a4a9ecbccdd4d33fbcc7c72cd9d8b5e115255
nnorukaemeka <nnorukaemeka@gmail.com>
2020-02-03 16:38:17 +0100

    validate route updated

--- app/models.py
+++ app/models.py
@@ -7,12 +7,13 @@ class PinGenerator(db.Model):
     """
     __tablename__ = 'pinGenerator'
 
-    pin = db.Column(db.String(15), primary_key=True, nullable=False, autoincrement=False, unique =True)
-    serial_no = db.Column(db.String(25), nullable=False, unique =True)
+    serial_no = db.Column(db.Integer(12), primary_key=True, nullable=False, autoincrement=True, unique =True)
+    pin = db.Column(db.String(15), nullable=False, unique =True)
 
     def __init__(self, pin, serial_no):
-        self.pin = pin
         self.serial_no = serial_no
+        self.pin = pin
+        
 
     def __repr__(self):
-        return '<title {}'.format(self.serial_no)
+        return '<title {}'.format(self.pin)
--- app/views.py
+++ app/views.py
@@ -19,70 +19,54 @@ def random_pin(length):
 #api route for generating and returning pin and s/n
 class Generate(Resource):
     def get(self):
-        try:
-            #generate a random uuid 15 digits
-            epin = random_pin(15)
-            #generate a random uuid 25 digits
-            eserial_no = random_pin(25)
-            #make an object of the database class
-            new_pin = PinGenerator(pin = epin, serial_no = eserial_no)
-
-            #compare the generated digits with database, if any exists, generate again
-            if PinGenerator.query.get(epin) or PinGenerator.query.filter_by(serial_no= eserial_no).first():
-                db.session.rollback()
-                return redirect(url_for('generate'))
-            # if they don't exist in db, add to database and return the generated digits
-            db.session.add(new_pin)
-            db.session.commit()
-            return {"pin": epin, "s/n": eserial_no}, 200
-        except exc.IntegrityError: #for any other exception, flag error message
-            db.session.rollback()
-            return {"message": " Oops! try again later"}, 404
-
+        epin = random_pin(15)   #generate a random uuid 15 digits
+        new_pin = PinGenerator(pin = epin) #post to table
+        db.session.add(new_pin) #add to database
+        db.session.commit() #commit to database
+        record = PinGenerator.query.filter_by(pin=epin).first()
+        return {"pin": record.pin, "s/n": record.serial_no}, 200
 api.add_resource(Generate, '/generate')  
 
 
 #api route for validating pin and s/n
-class ValidatePin(Resource):
-    def get(self,pin):
-        #get pin from user, and convert to string
-        epin = str(pin)
-        #check if  pin exist in the database
-        chkpin = PinGenerator.query.filter_by(pin=epin).first()
-        if chkpin: # if it exists, return 1 connoting valid
-            return {"message":"1"},200
-        else: # if it doesn't exist, return 0 connoting invalid
-            return {"message":"0"}, 200
-
-api.add_resource(ValidatePin, '/validate/pin/<string:pin>')
-
 class ValidateSn(Resource):
     def get(self,sn):
-        #get serial_no  from user, and convert to string
-        eserial = str(sn)
-        #check if serial_no  exist in the database
-        chkpin = PinGenerator.query.filter_by(serial_no=eserial).first()
-        if chkpin: # if it exists, return 1 connoting valid
-            return {"message":"1"},200
-        else: # if it doesn't exist, return 0 connoting invalid
-            return {"message":"0"}, 200
-
-api.add_resource(ValidateSn, '/validate/sn/<string:sn>')
+        #get pin from user, and convert to string
+        eserial = str(sn) 
+        #check if  serial_no exist in the database
+        checkpin = PinGenerator.query.filter_by(serial_no=eserial).first() 
+        # return 1 if valid else, return 0  
+        return {"message":"1"} if checkpin, else return {"message":"0"} 
+api.add_resource(ValidatePin, '/validate/sn/<string:sn>')
 
 
-#api route for returning all data in database
-class AllPin(Resource):
-    def get(self):
-        database1 = []
-        records = db.session.query(PinGenerator).all()
-        for record in records:
-            recordObject = {"pin": record.pin,
-                    "s/n": record.serial_no}
-            database1.append(recordObject)
+# class ValidateSn(Resource):
+#     def get(self,sn):
+#         #get serial_no  from user, and convert to string
+#         eserial = str(sn)
+#         #check if serial_no  exist in the database
+#         chkpin = PinGenerator.query.filter_by(serial_no=eserial).first()
+#         if chkpin: # if it exists, return 1 connoting valid
+#             return {"message":"1"},200
+#         else: # if it doesn't exist, return 0 connoting invalid
+#             return {"message":"0"}, 200
+
+# api.add_resource(ValidateSn, '/validate/sn/<string:sn>')
+
+
+# #api route for returning all data in database
+# class AllPin(Resource):
+#     def get(self):
+#         database1 = []
+#         records = db.session.query(PinGenerator).all()
+#         for record in records:
+#             recordObject = {"pin": record.pin,
+#                     "s/n": record.serial_no}
+#             database1.append(recordObject)
         
-        return {'Records': database1}, 200
+#         return {'Records': database1}, 200
 
-api.add_resource(AllPin, '/database')
+# api.add_resource(AllPin, '/database')
 
 
 #api route for the homepage
