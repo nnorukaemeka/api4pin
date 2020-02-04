6428c145063d6cdfbd39799d12bd734b18339851
nnorukaemeka <nnorukaemeka@gmail.com>
2020-02-04 17:44:48 +0100

    README updated

--- README.md
+++ README.md
@@ -30,5 +30,5 @@ flask run
 The api documentation is hosted as the homepage
 of the application.
 The purpose of this project is to build two(2) API. 
-This first endpoint [/generate] returns a pin and a serial_no(s/n). The second endpoint [/validate/sn/...] validates pin or s/n when posted by returning [1] as VALID or [0] as NOT VALID.
+This first endpoint [/generate] returns a pin and a serial_no(s/n). The second endpoint [/validate/<paste s/n>] validates pin or s/n when posted by returning [1] as VALID or [0] as NOT VALID.
 Feel free to consume the APIs.
--- app/views.py
+++ app/views.py
@@ -30,7 +30,7 @@ class ValidateSn(Resource):
         if (h[sn]) == None: #check if serial no is in hash table, 
             return {"message":"0"}  # return 0 if invalid 
         return {"message":"1"} #else, return 1 
-api.add_resource(ValidateSn, '/validate/sn/<string:sn>')
+api.add_resource(ValidateSn, '/validate/<string:sn>')
 
 
 
@@ -38,5 +38,5 @@ api.add_resource(ValidateSn, '/validate/sn/<string:sn>')
 class Home(Resource):
     def get(self):
         return {
-            'message': '''Welcome, my name is Emeka Nnoruka. The purpose of this project is to build two(2) API. This first endpoint [/generate] returns a pin and a serial_no(s/n). The second endpoint [/validate/sn/...] validates s/n when posted by returning [1] as VALID or [0] as NOT VALID. Feel free to consume the APIs.'''}
+            'message': '''Welcome, my name is Emeka Nnoruka. The purpose of this project is to build two(2) API. This first endpoint [/generate] returns a pin and a serial_no(s/n). The second endpoint [/validate/<paste s/n>] validates s/n when posted by returning [1] as VALID or [0] as NOT VALID. Feel free to consume the APIs.'''}
 api.add_resource(Home, '/')
